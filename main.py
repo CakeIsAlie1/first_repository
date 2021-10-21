93987e836fb829fa545ce189e8c8f2027c26c62d
торт ложь <92367852+CakeIsAlie1@users.noreply.github.com>
2021-10-21 15:52:20 +0500

    afdasefa
    swedfsaef

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-a = input("name: ")
-print("hi,")
-print("my name is misha")
+
+import time
+snake = ['@']
+snake.append("*")
+b = 1
+a = 1
+while a < 20:
+    for i in snake:
+        print(i, end="")
+    a = a + 1
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
+    snake.append("*")
+    time.sleep(1)
+while b < 22:
+    for i in snake:
+        print(i, end="")
+    b = b + 1
+    print("\n\n\n\n\n\n\n\n\n\n\n\n")
+    snake.pop()
+    time.sleep(1)
